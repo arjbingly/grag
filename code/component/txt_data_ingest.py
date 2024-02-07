4e0d3262c49e2ad3ba5ad5372d50bd6440739b37
Arjun Bingly <arjunbin@gmail.com>
2024-02-07 16:44:54 -0500

    Update txt_data_ingest to only contain load_and_splitter

--- code/component/txt_data_ingest.py
+++ code/component/txt_data_ingest.py
@@ -1,25 +1,5 @@
-import os
-from tqdm import tqdm
-
-import chromadb
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-
-# %%
-os.chdir('..')  # goes to ../CapStone_5/code
-os.chdir('..')  # goes to ../CapStone_5/
-print(os.getcwd())
-# %%
-## COFIGS
-VectorStorePath = "data/vectordb"
-CollectionName = "gutenberg"
-EmbeddingModel = "all-mpnet-base-v2"
-
-data_path = "data/Gutenberg/txt"
-
 
 # %%
 ## FUNCS
@@ -35,29 +15,3 @@ def load_split_dir(data_path):
                                                    is_separator_regex=False, )
     docs = loader.load_and_split(text_splitter)
     return docs
-
-def add_docs(docs, client, collection_name, embedding_function):
-    langchain_chroma = Chroma(client=client,
-                              collection_name=CollectionName,
-                              embedding_function=embedding_function, )
-
-    for doc in tqdm(docs, desc='Adding Documents:'):
-        _id = langchain_chroma.add_documents([doc])
-# %%
-docs = load_split_dir(data_path)
-print(f'Total Number of docs: {len(docs)}')
-# %%
-embedding_function = SentenceTransformerEmbeddings(model_name=EmbeddingModel)
-client = chromadb.PersistentClient(path=VectorStorePath)
-collection = client.get_or_create_collection(name=CollectionName)
-
-print('Before Adding Docs...')
-print(f'The {CollectionName} has {collection.count()} documenets')
-print(f'A few are..')
-print(collection.peek())
-
-add_docs(docs, client, CollectionName, embedding_function)
-print('After Adding Docs...')
-print(f'The {CollectionName} has {collection.count()} documents')
-print(f'A few are..')
-print(collection.peek())
