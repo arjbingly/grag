612c1d9ec47d70b3fb879a095d189c0805d6a5d9
Arjun Bingly <arjunbin@gmail.com>
2024-02-07 16:47:07 -0500

    Create config.py

---
+++ code/component/config.py
@@ -0,0 +1,9 @@
+
+chroma = {
+    'host' : 'localhost',
+    'port' : 8000,
+    'collection_name' : "gutenberg",
+    'embedding_model' : "all-mpnet-base-v2",
+    'store_path' : "data/vectordb",
+    'allow_reset': True
+}
