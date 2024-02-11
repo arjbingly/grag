151437b6c151d9690a8e2875350757ffb35700b5
sanchitvj <sanchitvj1026@gmail.com>
2024-02-11 18:00:47 -0500

    LLMs

---
+++ code/components/llama_cpp.py
@@ -0,0 +1,6 @@
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import LlamaCpp
+
