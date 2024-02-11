77b9187cde772201f77a2b57a09a18ad0eb39b1e
Arjun Bingly <arjunbin@gmail.com>
2024-02-11 00:16:37 -0500

    Reset script for vectordb and docstore

---
+++ code/component/reset_chroma.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+conda init bash
+conda activate Capstone5
+cd ..
+cd ..
+rm chroma.log
+cd data/vectordb
+rm -r *
+
---
+++ code/component/reset_store.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+conda init bash
+conda activate Capstone5
+cd ..
+cd ..
+cd data/docs
+rm -r *
+
