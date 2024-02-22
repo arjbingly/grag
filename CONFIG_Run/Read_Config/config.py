eb933b0b6115f098da98c3795b650c927e0993bb
sanchitvj <sanchitvj1026@gmail.com>
2024-02-22 18:57:12 -0500

    new script for config

--- CONFIG_Run/Read_Config/config.py
+++ CONFIG_Run/Read_Config/config.py
@@ -1,30 +1,9 @@
-import textwrap
 import os
 from configparser import ConfigParser, ExtendedInterpolation
 from pathlib import Path
 
 
-def wrap_text_preserve_newlines(text, width=110):
-    # Split the input text into lines based on newline characters
-    lines = text.split('\n')
-
-    # Wrap each line individually
-    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
-
-    # Join the wrapped lines back together using newline characters
-    wrapped_text = '\n'.join(wrapped_lines)
-
-    return wrapped_text
-
-
-def process_llm_response(llm_response):
-    # print(wrap_text_preserve_newlines(llm_response['result']))
-    print(llm_response['result'])
-    print('\nSources:')
-    for source in llm_response["source_documents"]:
-        print(source.metadata['source'])
-
-
+# Function to find the root directory
 def find_project_root(current_path):
     # Traverse up until you find the config.ini file
     while not (current_path / 'CONFIG_Run' / 'config' / 'config.ini').exists():
@@ -34,6 +13,7 @@ def find_project_root(current_path):
     return current_path
 
 
+# Function to get config object
 def get_config():
     # Assuming this script is somewhere inside your project directory
     script_location = Path(__file__).resolve()
@@ -47,3 +27,8 @@ def get_config():
     config.read(config_path)
 
     return config
+
+
+# Usage
+config = get_config()
+print(config['instance']['user'])  # Example of accessing a config value
