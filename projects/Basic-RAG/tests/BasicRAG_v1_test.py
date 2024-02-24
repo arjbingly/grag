423b0f461708d560a3d0df47f3d99533e022c9b7
Arjun Bingly <arjunbin@gmail.com>
2024-02-23 22:31:48 -0500

    Create BasicRAG_v1_test.py

---
+++ projects/Basic-RAG/tests/BasicRAG_v1_test.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+import sys
+
+# add Basic-RAG folder to sys path
+sys.path.insert(1, str(Path(os.getcwd()).parents[0]))
+
+from BasicRAG_v1 import call_rag
+
+
+if __name__ == "__main__":
+    query = 'What types of dependencies does dependence analysis identify in loop programs?'
+    call_rag(query)
