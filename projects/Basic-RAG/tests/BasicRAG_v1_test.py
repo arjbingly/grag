2540965538d92518b8b1d3e3a9d244f481b039c8
Arjun Bingly <arjunbin@gmail.com>
2024-02-26 16:30:40 -0500

    Reformat tests

--- projects/Basic-RAG/tests/BasicRAG_v1_test.py
+++ projects/Basic-RAG/tests/BasicRAG_v1_test.py
@@ -7,7 +7,6 @@ sys.path.insert(1, str(Path(os.getcwd()).parents[0]))
 
 from BasicRAG_v1 import call_rag
 
-
 if __name__ == "__main__":
     query = 'What types of dependencies does dependence analysis identify in loop programs?'
     response, sources = call_rag(query)
--- projects/Basic-RAG/tests/BasicRAG_v2_test.py
+++ projects/Basic-RAG/tests/BasicRAG_v2_test.py
@@ -7,7 +7,6 @@ sys.path.insert(1, str(Path(os.getcwd()).parents[0]))
 
 from BasicRAG_v2 import call_rag
 
-
 if __name__ == "__main__":
     query = 'What types of dependencies does dependence analysis identify in loop programs?'
     responses, sources = call_rag(query)
