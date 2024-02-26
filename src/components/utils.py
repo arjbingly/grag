89a1713e2230d22c051e3a0ef0981af8f16b814c
sanchitvj <sanchitvj1026@gmail.com>
2024-02-26 17:17:38 -0500

    restructred config file

--- src/components/utils.py
+++ src/components/utils.py
@@ -83,23 +83,45 @@ def load_prompt(json_file: str | os.PathLike, return_input_vars=False):
     return (prompt_template, input_vars) if return_input_vars else prompt_template
 
 
-def find_project_root(current_path):
-    # Traverse up until you find the config.ini file
-    while not (current_path / 'CONFIG_Run' / 'config' / 'config.ini').exists():
+def find_config_path(current_path: Path) -> Path:
+    """
+    Finds the path of the 'config.ini' file by traversing up the directory tree from the current path.
+
+    This function starts at the current path and moves up the directory tree until it finds a file named 'config.ini'.
+    If 'config.ini' is not found by the time the root of the directory tree is reached, a FileNotFoundError is raised.
+
+    Args:
+        current_path (Path): The starting point for the search, typically the location of the script being executed.
+
+    Returns:
+        Path: The path to the found 'config.ini' file.
+
+    Raises:
+        FileNotFoundError: If 'config.ini' cannot be found in any of the parent directories.
+    """
+    config_path = Path('src/config.ini')
+    while not (current_path / config_path).exists():
         current_path = current_path.parent
         if current_path == current_path.parent:
-            raise FileNotFoundError("config.ini not found in any parent directories.")
-    return current_path
+            raise FileNotFoundError(f"config.ini not found in {config_path}.")
+    return current_path / config_path
 
 
-def get_config():
+def get_config() -> ConfigParser:
+    """
+    Retrieves and parses the configuration settings from the 'config.ini' file.
+
+    This function locates the 'config.ini' file by calling `find_config_path` using the script's current location.
+    It initializes a `ConfigParser` object to read the configuration settings from the located 'config.ini' file.
+
+    Returns:
+        ConfigParser: A parser object containing the configuration settings from 'config.ini'.
+    """
     # Assuming this script is somewhere inside your project directory
     script_location = Path(__file__).resolve()
-    root_directory = find_project_root(script_location)
+    config_path = find_config_path(script_location)
 
-    # Path to your config file
-    config_path = root_directory / 'CONFIG_Run' / 'config' / 'config.ini'
-    print(config_path)
+    print(f"Loaded config from {config_path}.")
     # Initialize parser and read config
     config = ConfigParser(interpolation=ExtendedInterpolation())
     config.read(config_path)
