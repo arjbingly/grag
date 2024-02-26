175a73c380208a0b84f0ed72054cfa4780e3f704
sanchitvj <sanchitvj1026@gmail.com>
2024-02-26 16:53:40 -0500

    after rebasing with project-BasicRAG

--- src/components/utils.py
+++ src/components/utils.py
@@ -1,10 +1,16 @@
 import os
 import json
 import textwrap
+<<<<<<< HEAD
 from typing import List
 
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
+=======
+import os
+from configparser import ConfigParser, ExtendedInterpolation
+from pathlib import Path
+>>>>>>> eb933b0 (new script for config)
 
 
 def stuff_docs(docs: List[Document]) -> str:
@@ -60,6 +66,7 @@ def display_llm_output_and_sources(response_from_llm):
         print(source.metadata['source'])
 
 
+<<<<<<< HEAD
 def load_prompt(json_file: str | os.PathLike, return_input_vars=False):
     """
     Loads a prompt template from json file and returns a langchain ChatPromptTemplate
@@ -79,3 +86,27 @@ def load_prompt(json_file: str | os.PathLike, return_input_vars=False):
     input_vars = prompt_json['input_variables']
 
     return (prompt_template, input_vars) if return_input_vars else prompt_template
+
+
+def find_project_root(current_path):
+    # Traverse up until you find the config.ini file
+    while not (current_path / 'CONFIG_Run' / 'config' / 'config.ini').exists():
+        current_path = current_path.parent
+        if current_path == current_path.parent:
+            raise FileNotFoundError("config.ini not found in any parent directories.")
+    return current_path
+
+
+def get_config():
+    # Assuming this script is somewhere inside your project directory
+    script_location = Path(__file__).resolve()
+    root_directory = find_project_root(script_location)
+
+    # Path to your config file
+    config_path = root_directory / 'CONFIG_Run' / 'config' / 'config.ini'
+    print(config_path)
+    # Initialize parser and read config
+    config = ConfigParser(interpolation=ExtendedInterpolation())
+    config.read(config_path)
+
+    return config
