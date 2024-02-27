b51657b09b1d293623db9f5b2a84d03c0c398d39
sanchitvj <sanchitvj1026@gmail.com>
2024-02-26 20:31:39 -0500

    minimal changes

--- projects/Basic-RAG/BasicRAG-ingest_data.py
+++ projects/Basic-RAG/BasicRAG-ingest_data.py
@@ -1,12 +1,13 @@
-from pathlib import Path
 import os
+from pathlib import Path
 from uuid import UUID, uuid5
+
 from tqdm import tqdm
 
 from src.components.multivec_retriever import Retriever
 from src.components.parse_pdf import ParsePDF
-
 from src.components.utils import get_config
+
 config = get_config()
 
 DRY_RUN = True
@@ -58,11 +59,11 @@ def add_to_database(file_path, dry_run=False):
             retriever.add_docs(docs[format_key])
 
 
-# parser = ParsePDF()
-# retriever = Retriever()
-#
-# processed_files = set()
-# load_processed_files()  # Load processed files into the set on script startup
+parser = ParsePDF()
+retriever = Retriever()
+
+processed_files = set()
+load_processed_files()  # Load processed files into the set on script startup
 
 
 def main():
@@ -74,17 +75,14 @@ def main():
     for file in pbar:
         pbar.set_postfix({'Current file': file.relative_to(data_path)})
         pbar.write(add_file_to_database(file, dry_run=DRY_RUN))
-        # if str(file) not in processed_files:
-        #     add_to_database(file, dry_run=DRY_RUN)  # Add file to the vector database
-        #     processed_files.add(str(file))  # Add file_path to processed set
-        #     update_processed_file_record(str(file), dry_run=DRY_RUN)  # Update the processed file record file
-        #     pbar.write(f'Completed adding - {file.relative_to(data_path)}')
-        # else:
-        #     pbar.write(f'Already exists - {file.relative_to(data_path)}')
+        if str(file) not in processed_files:
+            add_to_database(file, dry_run=DRY_RUN)  # Add file to the vector database
+            processed_files.add(str(file))  # Add file_path to processed set
+            update_processed_file_record(str(file), dry_run=DRY_RUN)  # Update the processed file record file
+            pbar.write(f'Completed adding - {file.relative_to(data_path)}')
+        else:
+            pbar.write(f'Already exists - {file.relative_to(data_path)}')
 
 
 if __name__ == "__main__":
-    print(config['instance']['user'])
-    print(config['model']['temperature'])
-
-    # main()
+    main()
