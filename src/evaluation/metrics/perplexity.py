69b1d9d08e3b458cad61a03effc59c0a749a3c3c
sanchitvj <sanchitvj1026@gmail.com>
2024-03-15 17:04:22 -0400

    perplexity log

--- src/evaluation/metrics/perplexity.py
+++ src/evaluation/metrics/perplexity.py
@@ -10,4 +10,8 @@ response = llm.invoke(query)
 
 outs = llm.client(query, **llm._get_parameters())
 ppl = np.exp(-1 * np.mean(outs['choices'][0]['logprobs']['token_logprobs']))
-print("Perplexity: {}".format(ppl))
+H = -1 * np.mean(outs['choices'][0]['logprobs']['token_logprobs'])
+ppl_e = np.exp(H)
+ppl_2 = 2 ** H
+print("Perplexity: {}".format(ppl_e))
+print("Perplexity: {}".format(ppl_2))
