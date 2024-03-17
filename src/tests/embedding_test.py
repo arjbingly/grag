014e69714a3f1de14460ea825f8d302dcf960438
Arjun Bingly <arjunbin@gmail.com>
2024-03-17 16:47:28 -0400

    Update prompts and embedding test

--- src/tests/embedding_test.py
+++ src/tests/embedding_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from grag.components.embedding import Embedding
 
@@ -18,48 +19,20 @@ embedding_configs = [
     {'embedding_type': 'instructor-embedding',
      'embedding_model': 'hkunlp/instructor-xl', }
 ]
-# Test Documents
-docs = ['Dynamic programming is both a mathematical optimization method and an algorithmic paradigm. The method was '
-        'developed by Richard Bellman in the 1950s and has found applications in numerous fields, from aerospace '
-        'engineering to economics.',
-        'In computer science, recursion is a method of solving a computational problem where the solution depends on '
-        'solutions to smaller instances of the same problem. Recursion solves such recursive problems by using '
-        'functions that call themselves from within their own code. The approach can be applied to many types of '
-        'problems, and recursion is one of the central ideas of computer science.']
 
-print('Documents:')
-for i, doc in enumerate(docs):
-    print(f'{i + 1}. {doc}')
 
-query = 'What is recursion?'
-print(f'Query: {query}')
+@pytest.mark.parametrize('embedding_config', embedding_configs)
+def test_embeddings(embedding_config):
+    # docs tuple format: (doc, similar to doc, asimilar to doc)
+    doc_sets = [('The new movie is awesome.', 'The new movie is so great.', 'The video is awful'),
+                ('The cat sits outside.', 'The dog plays in the garden.', 'The car is parked inside')]
+    embedding = Embedding(**embedding_config)
+    for docs in doc_sets:
+        doc_vecs = [embedding.embedding_function.embed_query(doc) for doc in docs]
+        similarity_scores = [cosine_similarity(doc_vecs[0], doc_vecs[1]),
+                             cosine_similarity(doc_vecs[0], doc_vecs[2])]
+    assert similarity_scores[0] > similarity_scores[1]
 
-# testing first config
-print(f'Testing: {embedding_configs[0]}')
 
-embedding = Embedding(**embedding_configs[0])
-docs_vector = embedding.embedding_function.embed_documents(docs)
-query_vector = embedding.embedding_function.embed_query(query)
-
-similarity_scores = [cosine_similarity(query_vector, doc_vector) for doc_vector in docs_vector]
-print(f'Similairty Scores: {similarity_scores}')
-
-if similarity_scores[1] > similarity_scores[0]:
-    print('Test Passed')
-else:
-    print('Test Failed')
-
-# testing second config
-print(f'Testing: {embedding_configs[1]}')
-
-embedding = Embedding(**embedding_configs[1])
-docs_vector = embedding.embedding_function.embed_documents(docs)
-query_vector = embedding.embedding_function.embed_query(query)
-
-similarity_scores = [cosine_similarity(query_vector, doc_vector) for doc_vector in docs_vector]
-
-print(f'Similairty Scores: {similarity_scores}')
-if similarity_scores[1] > similarity_scores[0]:
-    print('Test Passed')
-else:
-    print('Test Failed')
+if __name__ == '__main__':
+    test_embeddings(embedding_configs[0])
