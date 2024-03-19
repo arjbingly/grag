75b479e96010184776b107b8d052e8e2e1f2478c
Arjun Bingly <arjunbin@gmail.com>
2024-03-19 12:40:06 -0400

    separate pipes in llm test

--- src/tests/llm_test.py
+++ src/tests/llm_test.py
@@ -22,14 +22,50 @@ hf_models = ['meta-llama/Llama-2-7b-chat',
 cpp_quantization = ['Q5_K_M', 'Q5_K_M', 'Q4_K_M', 'f16']
 hf_quantization = ['Q8', 'Q4', 'Q4', 'Q4']
 
-params = [(model, 'llama_cpp', quant) for model, quant in zip(llama_models, cpp_quantization)]
-params.extend([(model, 'hf', quant) for model, quant in zip(llama_models, cpp_quantization)])
+# params = [(model, 'llama_cpp', quant) for model, quant in zip(llama_models, cpp_quantization)]
+# params.extend([(model, 'hf', quant) for model, quant in zip(llama_models, cpp_quantization)])
+# 
+# 
+# @pytest.mark.parametrize("model_name, pipeline, quantization", params)
+# def test_model(model_name, pipeline, quantization):
+#     llm_ = LLM(quantization=quantization, model_name=model_name, pipeline=pipeline)
+#     model = llm_.load_model()
+#     response = model.invoke("Who are you?")
+#     assert isinstance(response, Text)
+#     del model
 
 
-@pytest.mark.parametrize("model_name, pipeline, quantization", params)
-def test_model(model_name, pipeline, quantization):
-    llm_ = LLM(quantization=quantization, model_name=model_name, pipeline=pipeline)
+params = [(model, quant) for model, quant in zip(llama_models, cpp_quantization)]
+
+
+@pytest.mark.parametrize("model_name, quantization", params)
+def test_llamacpp_pipe(model_name, quantization):
+    llm_ = LLM(quantization=quantization, model_name=model_name, pipeline='llama_cpp')
     model = llm_.load_model()
+    response = model.invoke("Who are you?")
+    assert isinstance(response, Text)
+    del model
+
+
+params = [(model, quant) for model, quant in zip(llama_models, cpp_quantization)]
+
+
+@pytest.mark.parametrize("model_name, quantization", params)
+def test_hf_local_pipe(model_name, quantization):
+    llm_ = LLM(quantization=quantization, model_name=model_name, pipeline='hf')
+    model = llm_.load_model()
+    response = model.invoke("Who are you?")
+    assert isinstance(response, Text)
+    del model
+
+
+params = [(model, quant) for model, quant in zip(hf_models, hf_quantization)]
+
+
+@pytest.mark.parametrize("hf_models, quantization", params)
+def test_hf_web_pipe(model_name, quantization):
+    llm_ = LLM(quantization=quantization, model_name=model_name, pipeline='hf')
+    model = llm_.load_model(is_local=False)
     response = model.invoke("Who are you?")
     assert isinstance(response, Text)
     del model
