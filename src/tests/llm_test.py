255112c5f1b2db48711346c178d06785038791ac
sanchitvj <sanchitvj1026@gmail.com>
2024-03-19 16:45:55 -0400

    all test cases passed

--- src/tests/llm_test.py
+++ src/tests/llm_test.py
@@ -3,50 +3,16 @@ from typing import Text
 import pytest
 from grag.components.llm import LLM
 
-#
-# models_to_test = ['Llama-2-7b-chat',
-#                   'Llama-2-13b-chat',
-#                   'Mixtral-8x7B-Instruct-v0.1',
-#                   'gemma-7b-it']
-#
-# pipeline_list = ['llama_cpp', 'hf']
-
 llama_models = ['Llama-2-7b-chat',
                 'Llama-2-13b-chat',
                 'Mixtral-8x7B-Instruct-v0.1',
                 'gemma-7b-it']
 hf_models = ['meta-llama/Llama-2-7b-chat-hf',
              'meta-llama/Llama-2-13b-chat-hf',
-             'mistralai/Mixtral-8x7B-Instruct-v0.1',
+             # 'mistralai/Mixtral-8x7B-Instruct-v0.1',
              'google/gemma-7b-it']
 cpp_quantization = ['Q5_K_M', 'Q5_K_M', 'Q4_K_M', 'f16']
-hf_quantization = ['Q8', 'Q4', 'Q4', 'Q4']
-
-# params = [(model, 'llama_cpp', quant) for model, quant in zip(llama_models, cpp_quantization)]
-# params.extend([(model, 'hf', quant) for model, quant in zip(llama_models, cpp_quantization)])
-# 
-# 
-# @pytest.mark.parametrize("model_name, pipeline, quantization", params)
-# def test_model(model_name, pipeline, quantization):
-#     llm_ = LLM(quantization=quantization, model_name=model_name, pipeline=pipeline)
-#     model = llm_.load_model()
-#     response = model.invoke("Who are you?")
-#     assert isinstance(response, Text)
-#     del model
-#
-#
-# params = [(model, quant) for model, quant in zip(llama_models, cpp_quantization)]
-#
-#
-# @pytest.mark.parametrize("model_name, quantization", params)
-# def test_hf_local_pipe(model_name, quantization):
-#     llm_ = LLM(quantization=quantization, model_name=model_name, pipeline='hf')
-#     model = llm_.load_model()
-#     response = model.invoke("Who are you?")
-#     assert isinstance(response, Text)
-#     del model
-
-
+hf_quantization = ['Q8', 'Q4', 'Q4']  # , 'Q4']
 params = [(model, quant) for model, quant in zip(hf_models, hf_quantization)]
 
 
