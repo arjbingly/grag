b90a8823d39215226b123553802efff0e9dd26d5
sanchitvj <sanchitvj1026@gmail.com>
2024-03-24 17:52:50 -0400

    quantizations all tests passed

--- src/grag/quantize/utils.py
+++ src/grag/quantize/utils.py
@@ -1,3 +1,5 @@
+"""Utility functions for quantization."""
+
 import os
 import subprocess
 from pathlib import Path
@@ -9,7 +11,7 @@ from huggingface_hub import snapshot_download
 config = get_config()
 
 
-def get_llamacpp_repo(root_path: str) -> subprocess.CompletedProcess:
+def get_llamacpp_repo(root_path: Union[str, Path]) -> subprocess.CompletedProcess:
     """Clones or pulls the llama.cpp repository into the specified root path.
 
     Args:
@@ -20,15 +22,27 @@ def get_llamacpp_repo(root_path: str) -> subprocess.CompletedProcess:
     """
     if os.path.exists(f"{root_path}/llama.cpp"):
         print(f"Repo exists at: {root_path}/llama.cpp")
-        res = subprocess.run(["git", "-C", f"{root_path}/llama.cpp", "pull"], check=True, capture_output=True)
+        res = subprocess.run(
+            ["git", "-C", f"{root_path}/llama.cpp", "pull"],
+            check=True,
+            capture_output=True,
+        )
     else:
-        res = subprocess.run(["git", "clone", "https://github.com/ggerganov/llama.cpp.git", f"{root_path}/llama.cpp"],
-                             check=True, capture_output=True)
+        res = subprocess.run(
+            [
+                "git",
+                "clone",
+                "https://github.com/ggerganov/llama.cpp.git",
+                f"{root_path}/llama.cpp",
+            ],
+            check=True,
+            capture_output=True,
+        )
 
     return res
 
 
-def building_llamacpp(root_path: str) -> None:
+def building_llamacpp(root_path: Union[str, Path]) -> None:
     """Attempts to build the llama.cpp project using make or cmake.
 
     Args:
@@ -36,24 +50,41 @@ def building_llamacpp(root_path: str) -> None:
     """
     os.chdir(f"{root_path}/llama.cpp/")
     try:
-        subprocess.run(['which', 'make'], check=True, stdout=subprocess.DEVNULL)
-        subprocess.run(['make', 'LLAMA_CUBLAS=1'], check=True)
-        print('Llama.cpp build successful.')
+        subprocess.run(["which", "make"], check=True, stdout=subprocess.DEVNULL)
+        subprocess.run(["make", "LLAMA_CUBLAS=1"], check=True)
+        print("Llama.cpp build successful.")
     except subprocess.CalledProcessError:
         try:
-            subprocess.run(['which', 'cmake'], check=True, stdout=subprocess.DEVNULL)
-            subprocess.run(['mkdir', 'build'], check=True)
+            subprocess.run(["which", "cmake"], check=True, stdout=subprocess.DEVNULL)
+            subprocess.run(["mkdir", "build"], check=True)
             subprocess.run(
-                ['cd', 'build', '&&', 'cmake', '..', '-DLLAMA_CUBLAS=ON', '&&', 'cmake', '--build', '.', '--config',
-                 'Release'], shell=True, check=True)
-            print('Llama.cpp build successful.')
+                [
+                    "cd",
+                    "build",
+                    "&&",
+                    "cmake",
+                    "..",
+                    "-DLLAMA_CUBLAS=ON",
+                    "&&",
+                    "cmake",
+                    "--build",
+                    ".",
+                    "--config",
+                    "Release",
+                ],
+                shell=True,
+                check=True,
+            )
+            print("Llama.cpp build successful.")
         except subprocess.CalledProcessError:
             print("Unable to build, cannot find make or cmake.")
     finally:
-        os.chdir(Path(__file__).parent)  # Assuming you want to return to the root path after operation
+        os.chdir(
+            Path(__file__).parent
+        )  # Assuming you want to return to the root path after operation
 
 
-def fetch_model_repo(repo_id: str, root_path: str) -> None:
+def fetch_model_repo(repo_id: str, root_path: Union[str, Path]) -> None:
     """Download model from huggingface.co/models.
 
     Args:
@@ -62,24 +93,33 @@ def fetch_model_repo(repo_id: str, root_path: str) -> None:
     """
     local_dir = f"{root_path}/llama.cpp/models/{repo_id.split('/')[1]}"
     os.makedirs(local_dir, exist_ok=True)
-    snapshot_download(repo_id=repo_id, local_dir=local_dir, local_dir_use_symlinks='auto', resume_download=True)
+    snapshot_download(
+        repo_id=repo_id,
+        local_dir=local_dir,
+        local_dir_use_symlinks="auto",
+        resume_download=True,
+    )
     print(f"Model downloaded in {local_dir}")
 
 
-def quantize_model(model_dir_path: str, quantization: str, root_path: str,
-                   output_dir: Optional[Union[str, Path]] = None) -> None:
+def quantize_model(
+    model_dir_path: Union[str, Path],
+    quantization: str,
+    root_path: Union[str, Path],
+    output_dir: Optional[Union[str, Path]] = None,
+) -> None:
     """Quantizes a specified model using a given quantization level.
 
     Args:
-        output_dir (str, optional): Directory to save quantized model. Defaults to None 
-        model_dir_path (str): The directory path of the model to be quantized.
+        output_dir (str, Path, optional): Directory to save quantized model. Defaults to None
+        model_dir_path (str, Path): The directory path of the model to be quantized.
         quantization (str): The quantization level to apply.
-        root_path (str): The root directory path of the project.
+        root_path (str, Path): The root directory path of the project.
     """
     os.chdir(f"{root_path}/llama.cpp/")
     model_dir_path = Path(model_dir_path)
     if output_dir is None:
-        output_dir = config['llm']['base_dir']
+        output_dir = config["llm"]["base_dir"]
 
     output_dir = Path(output_dir) / model_dir_path.name
     os.makedirs(output_dir, exist_ok=True)
@@ -87,6 +127,9 @@ def quantize_model(model_dir_path: str, quantization: str, root_path: str,
     subprocess.run(["python3", "convert.py", f"{model_dir_path}/"], check=True)
     model_file = model_dir_path / "ggml-model-f32.gguf"
     quantized_model_file = output_dir / f"ggml-model-{quantization}.gguf"
-    subprocess.run(["./quantize", str(model_file), str(quantized_model_file), quantization], check=True)
+    subprocess.run(
+        ["./quantize", str(model_file), str(quantized_model_file), quantization],
+        check=True,
+    )
     print(f"Quantized model present at {output_dir}")
     os.chdir(Path(__file__).parent)  # Return to the root path after operation
---
+++ src/tests/quantize/quantize_test.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+
+from grag.quantize.utils import (
+    building_llamacpp,
+    fetch_model_repo,
+    get_llamacpp_repo,
+    quantize_model,
+)
+
+root_path = Path(__file__).parent / 'test_data'
+os.makedirs(root_path, exist_ok=True)
+
+
+def test_get_llamacpp_repo():
+    get_llamacpp_repo(root_path)
+    repo_path = root_path / 'llama.cpp' / '.git'
+    assert os.path.exists(repo_path)
+
+
+def test_build_llamacpp():
+    building_llamacpp(root_path)
+    bin_path = root_path / 'llama.cpp' / 'quantize'
+    assert os.path.exists(bin_path)
+
+
+def test_fetch_model_repo():
+    fetch_model_repo('meta-llama/Llama-2-7b-chat', root_path)
+    model_dir_path = root_path / 'llama.cpp' / 'models' / 'Llama-2-7b-chat'
+    assert os.path.exists(model_dir_path)
+
+
+def test_quantize_model():
+    model_dir_path = root_path / 'llama.cpp' / 'models' / 'Llama-2-7b-chat'
+    quantize_model(model_dir_path, 'Q3_K_M', root_path, output_dir=model_dir_path.parent)
+    gguf_file_path = model_dir_path / "ggml-model-Q3_K_M.gguf"
+    assert os.path.exists(gguf_file_path)
