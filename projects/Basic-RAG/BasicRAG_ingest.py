a5b01cc53b4e6a0f5cba042f300dbfe4755cd4cc
Arjun Bingly <arjunbin@gmail.com>
2024-03-25 17:47:16 -0400

    linting and formatting cookbooks

--- projects/Basic-RAG/BasicRAG_ingest.py
+++ projects/Basic-RAG/BasicRAG_ingest.py
@@ -1,3 +1,5 @@
+"""A cookbook demonstrating how to ingest pdf files for use with BasicRAG."""
+
 from pathlib import Path
 
 from grag.components.multivec_retriever import Retriever
@@ -8,7 +10,7 @@ from grag.components.vectordb.deeplake_client import DeepLakeClient
 client = DeepLakeClient(collection_name="test")
 retriever = Retriever(vectordb=client)
 
-dir_path = Path(__file__).parent / 'some_dir'
+dir_path = Path(__file__).parent / "some_dir"
 
 retriever.ingest(dir_path)
 # rag = BasicRAG(doc_chain="refine")
