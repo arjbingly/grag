ee884c727b416463789c2ca295e0686e9d0e27c3
Arjun Bingly <arjunbin@gmail.com>
2024-03-25 17:38:14 -0400

    BasicRAG ingest cookbook

---
+++ projects/Basic-RAG/BasicRAG_ingest.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from grag.components.multivec_retriever import Retriever
+from grag.components.vectordb.deeplake_client import DeepLakeClient
+
+# from grag.rag.basic_rag import BasicRAG
+
+client = DeepLakeClient(collection_name="test")
+retriever = Retriever(vectordb=client)
+
+dir_path = Path(__file__).parent / 'some_dir'
+
+retriever.ingest(dir_path)
+# rag = BasicRAG(doc_chain="refine")
