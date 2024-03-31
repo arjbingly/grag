e683a1a8016b486aa449d674a8cc6d788c187dc5
sanchitvj <sanchitvj1026@gmail.com>
2024-03-31 18:33:56 -0400

    rewriting config.ini

--- ci/modify_config.py
+++ ci/modify_config.py
@@ -1,6 +1,9 @@
+import configparser
 import os
 
 from grag.components.utils import get_config
+
+config = configparser.ConfigParser()
 
 workspace = os.getenv('WORKSPACE')
 jenkins_home = os.getenv('JENKINS_HOME')
@@ -10,4 +13,6 @@ config['root']['root_path'] = f'{workspace}'
 config['data']['data_path'] = f'{jenkins_home}/ci_test_data/data'
 config['llm']['base_dir'] = f'{jenkins_home}/ci_test_models'
 
-config.write(f'{workspace}/src/config.ini')
+# config.write(f'{workspace}/src/config.ini')
+with open(f'{workspace}/src/config.ini', 'w') as configfile:
+    config.write(configfile)
