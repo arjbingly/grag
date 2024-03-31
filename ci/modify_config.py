33d553fa89fa52cb2a76ee3b88c7ae989b47ba60
sanchitvj <sanchitvj1026@gmail.com>
2024-03-31 18:03:12 -0400

    Jenkisfile, CI

---
+++ ci/modify_config.py
@@ -0,0 +1,13 @@
+import os
+
+from grag.components.utils import get_config
+
+workspace = os.getenv('WORKSPACE')
+jenkins_home = os.getenv('JENKINS_HOME')
+
+config = get_config()
+config['root']['root_path'] = f'{workspace}'
+config['data']['data_path'] = f'{jenkins_home}/ci_test_data/data'
+config['llm']['base_dir'] = f'{jenkins_home}/ci_test_models'
+
+config.write(f'{workspace}/src/config.ini')
