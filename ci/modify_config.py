8ad88fb3fc1ae1ae96fb7623b87515c421452d42
sanchitvj <sanchitvj1026@gmail.com>
2024-03-31 18:35:53 -0400

    model path modified

--- ci/modify_config.py
+++ ci/modify_config.py
@@ -11,7 +11,7 @@ jenkins_home = os.getenv('JENKINS_HOME')
 config = get_config()
 config['root']['root_path'] = f'{workspace}'
 config['data']['data_path'] = f'{jenkins_home}/ci_test_data/data'
-config['llm']['base_dir'] = f'{jenkins_home}/ci_test_models'
+config['llm']['base_dir'] = f'{jenkins_home}/ci_test_models/models'
 
 # config.write(f'{workspace}/src/config.ini')
 with open(f'{workspace}/src/config.ini', 'w') as configfile:
