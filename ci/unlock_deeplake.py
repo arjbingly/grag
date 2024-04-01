7fc88500ceb436933e243b2ebfbb5a9640be3944
sanchitvj <sanchitvj1026@gmail.com>
2024-04-01 19:04:28 -0400

    .

--- ci/unlock_deeplake.py
+++ ci/unlock_deeplake.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from pathlib import Path
 
 jenkins_home = os.getenv('JENKINS_HOME')
@@ -6,5 +7,5 @@ jenkins_home = os.getenv('JENKINS_HOME')
 lock_path = Path(jenkins_home) / 'ci_test_data/data/vectordb/ci_test/dataset_lock.lock'
 
 if os.path.exists(lock_path):
-    os.remove(lock_path)
+    shutil.rmtree(lock_path)
     print('Deleting lock file: {}'.format(lock_path))
