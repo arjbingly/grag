076ebade4850192c121d16e021191c4cfc22b4d9
sanchitvj <sanchitvj1026@gmail.com>
2024-04-01 16:04:56 -0400

    failing build because of environment

--- cookbook/Basic-RAG/BasicRAG_ingest.py
+++ cookbook/Basic-RAG/BasicRAG_ingest.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from grag.components.multivec_retriever import Retriever
 from grag.components.vectordb.deeplake_client import DeepLakeClient
 
-client = DeepLakeClient(collection_name="test")
+client = DeepLakeClient(collection_name="ci_test")
 retriever = Retriever(vectordb=client)
 
-dir_path = Path(__file__).parents[2] / "data/client_test/test/"
+dir_path = Path(__file__).parents[2] / "data/ci_test/"
 
 retriever.ingest(dir_path)
