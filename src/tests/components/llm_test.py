7324f5ee8cb997f34065eefb3f18e4f9449bec98
sanchitvj <sanchitvj1026@gmail.com>
2024-04-03 19:17:27 -0400

    .

--- src/tests/components/llm_test.py
+++ src/tests/components/llm_test.py
@@ -11,18 +11,15 @@ llama_models = [
     "Llama-2-13b-chat",
     "gemma-7b-it",
     "Mixtral-8x7B-Instruct-v0.1",
-    # "gemma-7b-it",
 ]
 hf_models = [
     "meta-llama/Llama-2-7b-chat-hf",
     "meta-llama/Llama-2-13b-chat-hf",
-    # 'mistralai/Mixtral-8x7B-Instruct-v0.1',
     "google/gemma-7b-it",
 ]
 cpp_quantization = ["Q5_K_M", "Q5_K_M", "f16", "Q4_K_M"]
-# cpp_quantization = ["Q5_K_M", "f16", "Q4_K_M"]  # , "f16"]
 gpu_layers = ['-1', '-1', '18', '18']
-hf_quantization = ["Q8", "Q4", "Q4"]  # , 'Q4']
+hf_quantization = ["Q8", "Q4", "Q4"]
 params = [(model, quant) for model, quant in zip(hf_models, hf_quantization)]
 
 
@@ -34,13 +31,14 @@ def test_hf_web_pipe(hf_models, quantization):
     assert isinstance(response, Text)
     del model
 
-# params = [(model, gpu_layer, quant) for model, gpu_layer, quant in zip(llama_models, gpu_layers, cpp_quantization)]
-# 
-# 
-# @pytest.mark.parametrize("model_name, gpu_layer, quantization", params)
-# def test_llamacpp_pipe(model_name, gpu_layer, quantization):
-#     llm_ = LLM(quantization=quantization, model_name=model_name, n_gpu_layers=gpu_layer, pipeline="llama_cpp")
-#     model = llm_.load_model()
-#     response = model.invoke("Who are you?")
-#     assert isinstance(response, Text)
-#     del model
+
+params = [(model, gpu_layer, quant) for model, gpu_layer, quant in zip(llama_models, gpu_layers, cpp_quantization)]
+
+
+@pytest.mark.parametrize("model_name, gpu_layer, quantization", params)
+def test_llamacpp_pipe(model_name, gpu_layer, quantization):
+    llm_ = LLM(quantization=quantization, model_name=model_name, n_gpu_layers=gpu_layer, pipeline="llama_cpp")
+    model = llm_.load_model()
+    response = model.invoke("Who are you?")
+    assert isinstance(response, Text)
+    del model
