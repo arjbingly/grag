76c2db140e6205cbea0e09fa2fa1ca729ecb6811
Arjun Bingly <arjunbin@gmail.com>
2024-04-13 17:12:36 -0400

    Add threshold

--- gnn/data.py
+++ gnn/data.py
@@ -11,16 +11,19 @@ from tqdm import tqdm
 from gnn.utils import cosine_similarity
 
 
-def gen_egdes(embeddings, verbose=True):
+def gen_egdes(embeddings, verbose=True, threshold=0.5):
     """Generates edges and edge features for a graph based on pairwise cosine similarity of embeddings.
 
     Args:
         embeddings (list): A list of embeddings for each document.
+        verbose (bool, optional): If True, prints out the edges and edge features for each document. Defaults to True.
+        threshold (float, optional): Threshold for cosine similarity. Defaults to 0.5.
 
     Returns:
         tuple: A tuple containing two NumPy arrays:
             - edges: An array of shape (2, num_edges) containing the indices of connected nodes.
             - edge_features: An array of shape (num_edges, 1) containing the cosine similarities between connected nodes.
+            
 
     Example:
         >>> embeddings = [np.array([0.1, 0.2, 0.3]), np.array([0.4, 0.5, 0.6]), np.array([0.7, 0.8, 0.9])]
@@ -34,9 +37,9 @@ def gen_egdes(embeddings, verbose=True):
         pbar = combinations(enumerate(embeddings), 2)
     for doc1, doc2 in pbar:
         similarity = cosine_similarity(doc1[1], doc2[1])
-
-        edges.append((doc1[0], doc2[0]))
-        edge_features.append([similarity])
+        if similarity > threshold:
+            edges.append((doc1[0], doc2[0]))
+            edge_features.append([similarity])
     edges = np.array(edges).T
     edge_features = np.array(edge_features)
     return edges, edge_features
