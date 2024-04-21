4a7a391c431aa636b99288d2997bd2e5fcf043f5
Arjun Bingly <arjunbin@gmail.com>
2024-04-21 14:13:36 -0400

    SageConv training - save config

--- gnn/sageconv_gae_train.py
+++ gnn/sageconv_gae_train.py
@@ -16,44 +16,49 @@ from gnn.sageconv_encoder import SageConvEncoder
 from gnn.utils import test, train
 
 # Args
-data_filepath = 'Data/eg_data.json'
-with_labels = True
-num_epochs = 2000
+# Reproducibility conf
+random_seed = 1505
+# Data conf
+data_filepath = 'Data/cranfield.json'
+with_labels = False
+# Random Link Split conf
+num_val = 0.1
+num_test = 0.1
+neg_sampling_ratio = 0.3
+# Model conf
 encoder_hidden_channels = 64
 encoder_dropout_prob = 0.2
 encoder_hidden_channels = [128, 64]
 encoder_dropout_prob = [0.5, 0.2]
 encoder_out_channels = 32
 decoder_hidden_channels = [encoder_out_channels * 2, 32]
-# encoder_hidden_channels = [64, 32]
-# decoder_hidden_channels = [64, 32]
+# Training conf
 lr = 1e-3
-num_val = 0.1
-num_test = 0.1
-random_seed = 1505
+num_epochs = 3000
+early_stop_n_epochs = 50
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model_save_dir = Path(f'{__file__}').parent / Path('models')
 os.makedirs(model_save_dir, exist_ok=True)
-model_name = Path(f'SageConv_eg_{datetime.now().strftime("%Y_%m_%d_%H%M")}.pt')
-model_save_path = model_save_dir / model_name
+model_name = Path(f'SageConv_cranfield_{datetime.now().strftime("%Y_%m_%d_%H%M")}')
+model_save_path = model_save_dir / f'{model_name}.pt'
+config_save_path = model_save_dir / f'{model_name}.json'
 
-
-def main():
+if __name__ == '__main__':
     if random_seed is not None:
         torch.manual_seed(random_seed)
 
     with open(data_filepath, 'r') as f:
         data_dict = json.load(f)
 
-    data = gen_data(data_dict, with_label)
+    data = gen_data(data_dict, with_labels=with_labels)
 
     # split the data
     train_data, val_data, test_data = T.RandomLinkSplit(
         num_val=num_val,
         num_test=num_test,
-        neg_sampling_ratio=0.3,
+        neg_sampling_ratio=neg_sampling_ratio,
         add_negative_train_samples=True,
         is_undirected=True
     )(data)
@@ -77,9 +82,10 @@ def main():
     model = model.to(device)
     print(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
     # Training
     pbar = tqdm(range(num_epochs), desc='Training', unit="Epoch")
+
+    train_start_time = datetime.now()
     for epoch in pbar:
         loss = train(model, train_data, optimizer, with_labels=with_labels)
         train_rmse = test(model, train_data, with_labels=with_labels)
@@ -89,15 +95,59 @@ def main():
                                        "Val": f"{val_rmse:.4f}"})
         # print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_rmse:.4f}, '
         #       f'Val: {val_rmse:.4f}')
-
+        # Early Stopping
+        if epoch == 0:
+            prev_val_rmse = val_rmse.detach().cpu().numpy()
+            early_stop_epochs = 0
+        else:
+            if prev_val_rmse >= val_rmse.detach().cpu().numpy():
+                early_stop_epochs += 1
+                if early_stop_epochs >= early_stop_n_epochs:
+                    print(f'Early stopping at epoch {epoch}')
+                    break
+            else:
+                early_stop_epochs = 0
+                prev_val_rmse = val_rmse.detach().cpu().numpy()
+    train_end_time = datetime.now()
     # Eval
     test_rmse, pred = test(model, test_data, return_pred=True)
     pred = pred.numpy()
     target = test_data.cpu().edge_label.squeeze(-1).numpy()
 
-    # Save model
+    # Save model and config
     torch.save(model.state_dict(), model_save_path)
 
-
-if __name__ == '__main__':
-    main()
+    model_conf = {
+        'reproducibility_conf': {
+            'random_seed': 1505,
+        },
+        'data_conf': {
+            'data_filepath': 'Data/eg_data.json',
+            'with_labels': True,
+        },
+        'data_split_conf': {
+            'num_val': 0.1,
+            'num_test': 0.1,
+            'neg_sampling_ratio': 0.3,
+        },
+        'model_conf': {
+            'model': f'{model}',
+            'encoder_hidden_channels': [128, 64],
+            'encoder_dropout_prob': [0.5, 0.2],
+            'encoder_out_channels': 32,
+            'decoder_hidden_channels': [encoder_out_channels * 2, 32],
+        },
+        'train_conf': {
+            'num_epochs': 2000,
+            'lr': 1e-3,
+            'early_stop_n_epochs': 10,
+        },
+        'train_log': {
+            'start_time': f'{train_start_time}',
+            'end_time': f'{train_end_time}',
+            'time_taken': f'{train_end_time - train_start_time}',
+            'epochs': epoch,
+        }
+    }
+    with open(config_save_path, 'w+') as f:
+        json.dump(model_conf, f, indent=4)
