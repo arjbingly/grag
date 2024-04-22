f20e42d64e4a8e366f7b53e5ca62a0ba735b1704
Arjun Bingly <arjunbin@gmail.com>
2024-04-22 18:33:38 -0400

    InnerProductDecoder

--- gnn/sageconv_gae_train.py
+++ gnn/sageconv_gae_train.py
@@ -1,17 +1,17 @@
 """This script contains code to train and evaluate a GNN model for edge prediction."""
 
-import json
 import os
 from datetime import datetime
 from pathlib import Path
 
 import torch
 import torch_geometric.transforms as T
+from torch_geometric.nn.models import InnerProductDecoder
+from torchmetrics.classification import BinaryAccuracy
 from tqdm import tqdm
 
-from gnn.data import gen_data
+from gnn.data import get_data
 from gnn.GAE import GAEModel
-from gnn.linear_decoder import LinearEdgeDecoder
 from gnn.sageconv_encoder import SageConvEncoder
 from gnn.utils import EarlyStopping, SaveBestModel, test, train
 
@@ -20,22 +20,27 @@ from gnn.utils import EarlyStopping, SaveBestModel, test, train
 random_seed = 1505
 model_name = Path('SageConv_test_data')
 # Data conf
-data_filepath = 'Data/cranfield.json'
+data_name = 'cranfield'
+# data_filepath = 'Data/cranfield.json'
 with_labels = False
+threshold = 0.8
 # Random Link Split conf
 num_val = 0.1
 num_test = 0.1
-neg_sampling_ratio = 0.3
+neg_sampling_ratio = 4
+
 # Model conf
-encoder_hidden_channels = [128, 64]
-encoder_dropout_prob = [0.5, 0.2]
+encoder_hidden_channels = [128, 64, 32]
+encoder_dropout_prob = [0.5, 0.2, 0.1]
 encoder_out_channels = 32
 decoder_hidden_channels = [encoder_out_channels * 2, 32]
 # Training conf
 lr = 1e-3
-num_epochs = 3000
-early_stop_patience = 15
+num_epochs = 10000
+early_stop_patience = 200
 early_stop_delta = 0.0001
+
+data_dir = Path(f'{__file__}').parent / 'Data'
 
 model_save_dir = Path(f'{__file__}').parent / Path('models')
 os.makedirs(model_save_dir, exist_ok=True)
@@ -45,10 +50,11 @@ if __name__ == '__main__':
     if random_seed is not None:
         torch.manual_seed(random_seed)
 
-    with open(data_filepath, 'r') as f:
-        data_dict = json.load(f)
+    data = get_data(data_name, with_labels, threshold, data_dir)
 
-    data = gen_data(data_dict, with_labels=with_labels)
+    split_data = data.clone()
+    if with_labels:
+        del (split_data.edge_label)
 
     # split the data
     train_data, val_data, test_data = T.RandomLinkSplit(
@@ -57,10 +63,11 @@ if __name__ == '__main__':
         neg_sampling_ratio=neg_sampling_ratio,
         add_negative_train_samples=True,
         is_undirected=True
-    )(data)
+    )(split_data)
     print(f'{train_data=}')
     print(f'{val_data=}')
     print(f'{test_data=}')
+
     train_data = train_data.to(device)
     val_data = val_data.to(device)
     test_data = test_data.to(device)
@@ -68,7 +75,7 @@ if __name__ == '__main__':
     encoder = SageConvEncoder(hidden_channels=encoder_hidden_channels,
                               out_channels=encoder_out_channels,
                               dropout_probs=encoder_dropout_prob)
-    decoder = LinearEdgeDecoder(hidden_channels=decoder_hidden_channels)
+    decoder = InnerProductDecoder()
     encoder = encoder.to(device)
     decoder = decoder.to(device)
     model = GAEModel(
@@ -88,14 +95,17 @@ if __name__ == '__main__':
     else:
         criterion = torch.nn.BCEWithLogitsLoss()
         pre_process = None
+        metric = BinaryAccuracy().to(device)
 
     model_conf = {
         'reproducibility_conf': {
             'random_seed': random_seed,
         },
         'data_conf': {
-            'data_filepath': data_filepath,
+            'data_name': data_name,
+
             'with_labels': with_labels,
+            'threshold': threshold,
         },
         'data_split_conf': {
             'num_val': num_val,
@@ -118,15 +128,15 @@ if __name__ == '__main__':
     }
     # Training
     pbar = tqdm(range(num_epochs), desc='Training', unit="Epoch")
-    saver = SaveBestModel(save_dir=model_save_dir, model_name=model_name, pbar=pbar)
-    early_stopper = EarlyStopping(patience=early_stop_patience, delta=early_stop_delta, pbar=pbar)
+    saver = SaveBestModel(save_dir=model_save_dir, model_name=model_name, pbar=pbar, is_loss=False)
+    early_stopper = EarlyStopping(patience=early_stop_patience, delta=early_stop_delta, pbar=pbar, is_loss=False)
     train_start_time = datetime.now()
     for epoch in pbar:
         if early_stopper.stop:
             break
         loss = train(model, train_data, optimizer, criterion)
-        train_loss = test(model, train_data, criterion, pre_process_func=pre_process)
-        val_loss = test(model, val_data, criterion, pre_process_func=pre_process)
+        train_loss = test(model, train_data, metric, pre_process_func=pre_process)
+        val_loss = test(model, val_data, metric, pre_process_func=pre_process)
         pbar.set_postfix(ordered_dict={"Loss": f"{loss: .4f}",
                                        "Train": f"{train_loss: .4f}",
                                        "Val": f"{val_loss:.4f}"})
@@ -144,9 +154,9 @@ if __name__ == '__main__':
 
     # Eval
     model.load_state_dict(torch.load(saver.best_model_path)['model_state_dict'])
-    train_loss = test(model, train_data, criterion, pre_process_func=pre_process)
-    val_loss = test(model, val_data, criterion, pre_process_func=pre_process)
-    test_loss = test(model, test_data, criterion, pre_process_func=pre_process)
+    train_loss = test(model, train_data, metric, pre_process_func=pre_process)
+    val_loss = test(model, val_data, metric, pre_process_func=pre_process)
+    test_loss = test(model, test_data, metric, pre_process_func=pre_process)
     print('*** Best Model ***')
     print(f' Train loss: {train_loss:.4f}')
     print(f' Val   loss: {val_loss:.4f}')
